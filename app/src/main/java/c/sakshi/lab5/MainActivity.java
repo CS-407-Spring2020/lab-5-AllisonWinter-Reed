9e8452c81aef1c50818b5abc96c4addc3ee4e8cd
Allison Winter-Reed <38668004+AllisonWinter-Reed@users.noreply.github.com>
2020-03-01 13:14:45 -0600

    Mid Milestone 1

--- app/src/main/java/c/sakshi/lab5/MainActivity.java
+++ app/src/main/java/c/sakshi/lab5/MainActivity.java
@@ -1,7 +1,11 @@
 package c.sakshi.lab5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void loginClick(View view) {
+        EditText username = (EditText) findViewById(R.id.username);
+        String userStr = username.getText().toString();
+
+        secondScreen(userStr);
+    }
+
+    private void secondScreen(String username) {
+        Intent intent = new Intent(this, SecondScreen.class);
+        intent.putExtra("username", username);
+
+        startActivity(intent);
+    }
+
 }
